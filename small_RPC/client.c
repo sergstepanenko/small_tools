62f368c63ed35d6c6b5a8458f67c11ea8afff8fd
Sergii <Sergii.Stepanenko@malighting.de>
2018-02-22 14:34:57 +0100

    New version of RPC

--- small_RPC/client.c
+++ small_RPC/client.c
@@ -9,103 +9,99 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#define MAX_STRING_LENGTH 512
 /* Adresse für multicast IP */
 static char *host_name = "224.0.33.33";
 static int port = 33333;
 static struct ip_mreq command;
-static int setup_multicast_socket (void) 
+static int setup_multicast_socket(void) 
 {
     int loop = 1;
     int socket_descriptor;
     struct sockaddr_in sin;
-    memset (&sin, 0, sizeof (sin));
+    memset(&sin, 0, sizeof(sin));
     sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = htonl (INADDR_ANY);
-    sin.sin_port = htons (port);
-    if ( ( socket_descriptor = socket(PF_INET, SOCK_DGRAM, 0)) == -1) 
+    sin.sin_addr.s_addr = htonl(INADDR_ANY);
+    sin.sin_port = htons(port);
+    if ((socket_descriptor = socket(PF_INET, SOCK_DGRAM, 0)) == -1) 
     {
-        perror ("socket()");
-        exit (EXIT_FAILURE);
+        perror("socket()");
+        exit(EXIT_FAILURE);
     }
     /* Mehr Prozessen erlauben, denselben Port zu nutzen */
     loop = 1;
-    if (setsockopt ( socket_descriptor,
-                    SOL_SOCKET,
-                    SO_REUSEADDR,
-                    &loop, sizeof (loop)) < 0) 
+    if(setsockopt(socket_descriptor,
+                SOL_SOCKET,
+                SO_REUSEADDR,
+                &loop, sizeof(loop)) < 0) 
     {
-        perror ("setsockopt:SO_REUSEADDR");
-        exit (EXIT_FAILURE);
+        perror("setsockopt:SO_REUSEADDR");
+        exit(EXIT_FAILURE);
     }
-    if(bind( socket_descriptor,
+    if(bind(socket_descriptor,
             (struct sockaddr *)&sin,
             sizeof(sin)) < 0) 
     {
-        perror ("bind");
-        exit (EXIT_FAILURE);
+        perror("bind");
+        exit(EXIT_FAILURE);
     }
     /* Broadcast auf dieser Maschine zulassen */
     loop = 1;
-    if (setsockopt ( socket_descriptor,
-                    IPPROTO_IP,
-                    IP_MULTICAST_LOOP,
-                    &loop, sizeof (loop)) < 0) 
+    if(setsockopt(socket_descriptor,
+                IPPROTO_IP,
+                IP_MULTICAST_LOOP,
+                &loop, sizeof(loop)) < 0) 
     {
-        perror ("setsockopt:IP_MULTICAST_LOOP");
-        exit (EXIT_FAILURE);
+        perror("setsockopt:IP_MULTICAST_LOOP");
+        exit(EXIT_FAILURE);
     }
     /* Join the broadcast group: */
-    command.imr_multiaddr.s_addr = inet_addr ("224.0.33.33");
-    command.imr_interface.s_addr = htonl (INADDR_ANY);
-    if (command.imr_multiaddr.s_addr == -1) 
+    command.imr_multiaddr.s_addr = inet_addr("224.0.33.33");
+    command.imr_interface.s_addr = htonl(INADDR_ANY);
+    if(command.imr_multiaddr.s_addr == -1) 
     {
-        perror ("224.0.33.33 ist keine Multicast-Adresse\n");
-        exit (EXIT_FAILURE);
+        perror("224.0.33.33 ist keine Multicast-Adresse\n");
+        exit(EXIT_FAILURE);
     }
-    if (setsockopt ( socket_descriptor,
-                    IPPROTO_IP,
-                    IP_ADD_MEMBERSHIP,
-                    &command, sizeof (command)) < 0) 
+    if(setsockopt(socket_descriptor,
+                IPPROTO_IP,
+                IP_ADD_MEMBERSHIP,
+                &command, sizeof(command)) < 0) 
     {
-        perror ("setsockopt:IP_ADD_MEMBERSHIP");
+        perror("setsockopt:IP_ADD_MEMBERSHIP");
     }
     return socket_descriptor;
 }
-int main (void) 
+int main(void) 
 {
-    int iter = 0;
     int sin_len;
-    char message;
+    char message[MAX_STRING_LENGTH];
     int socket;
     struct sockaddr_in sin;
     struct hostent *server_host_name;
-    if ((server_host_name = gethostbyname (host_name)) == 0) 
+    if((server_host_name = gethostbyname(host_name)) == 0) 
     {
-        perror ("gethostbyname");
-        exit (EXIT_FAILURE);
+        perror("gethostbyname");
+        exit(EXIT_FAILURE);
     }
-    socket = setup_multicast_socket ();
+    socket = setup_multicast_socket();
     /* Broadcast-Nachrichten empfangen */
-    while (iter++ < 10) 
+    
+    sin_len = sizeof(sin);
+    if(recvfrom(socket, message, MAX_STRING_LENGTH, 0,
+                (struct sockaddr *) &sin, &sin_len)==-1) 
     {
-        sin_len = sizeof (sin);
-        if (recvfrom(socket, &message, 1, 0,
-                    (struct sockaddr *) &sin, &sin_len)==-1) 
-        {
-            perror ("recvfrom");
-        }
-        printf ("Antwort #%-2d vom Server: %d\n",
-        iter, message);
-        sleep (2);
+        perror("recvfrom");
     }
+    printf("%s\n", message);
+    
     /* Multicast-Socket aus der Gruppe entfernen */
-    if (setsockopt ( socket,
-                    IPPROTO_IP,
-                    IP_DROP_MEMBERSHIP,
-                    &command, sizeof (command)) < 0 ) 
+    if(setsockopt(socket, IPPROTO_IP,
+                IP_DROP_MEMBERSHIP,
+                &command, sizeof(command)) < 0 ) 
     {
-        perror ("setsockopt:IP_DROP_MEMBERSHIP");
+        perror("setsockopt:IP_DROP_MEMBERSHIP");
     }
-    close (socket);
+    close(socket);
     return EXIT_SUCCESS;
 }
--- small_RPC/server.c
+++ small_RPC/server.c
@@ -10,62 +10,33 @@
 #include <string.h>
 #include <errno.h>
 static int port = 33333;
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
     int socket_descriptor;
     struct sockaddr_in address;
-    
-    int index;
-    int c;
-    
-    char msg = 0;
-    // 0 - no action
-    // 1 - reboot system
-    // 2 - shutdown system
-    // 3 - kill application
-
-    opterr = 0;
-
-    while ((c = getopt (argc, argv, "rsk")) != -1)
-        switch (c)
-        {
-        case 'r':
-            msg = 1;
-            break;
-        case 's':
-            msg = 2;
-            break;
-        case 'k':
-            msg = 3;
-            break;
-        case '?':
-            return 1;
-        default:
-            abort ();
-        }
-
-    //for (index = optind; index < argc; index++)
-        //printf ("Non-option argument %s\n", argv[index]);
-    
-    socket_descriptor = socket (AF_INET, SOCK_DGRAM, 0);
-    if (socket_descriptor == -1) 
+        
+    if(argv[1])
     {
-        perror ("socket()");
-        exit (EXIT_FAILURE);
-    }
-    memset (&address, 0, sizeof (address));
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = inet_addr ("224.0.33.33");
-    address.sin_port = htons (port);
-    while (1) 
-    {
-        if (sendto(socket_descriptor, &msg, sizeof (msg),
-                    0, (struct sockaddr *) &address, sizeof (address)) < 0) 
+        printf("%s\n", argv[1]);
+    
+        socket_descriptor = socket(AF_INET, SOCK_DGRAM, 0);
+        if(socket_descriptor == -1) 
         {
-        perror ("sendto()");
-        exit (EXIT_FAILURE);
+            perror("socket()");
+            exit(EXIT_FAILURE);
         }
-        sleep (10);
+        memset(&address, 0, sizeof(address));
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = inet_addr("224.0.33.33");
+        address.sin_port = htons(port);
+
+        if (sendto(socket_descriptor, argv[1], strlen(argv[1]),
+                    0, (struct sockaddr *) &address, sizeof(address)) < 0) 
+        {
+        perror("sendto()");
+        exit(EXIT_FAILURE);
+        }
     }
+
     return EXIT_SUCCESS;
 }
